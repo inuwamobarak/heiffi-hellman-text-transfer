60d2624220b5444dfd8b64a6d843252ad0db4c14
Inuwa Mobarak Abraham <inuwamobarak@gmail.com>
2023-04-04 07:20:11 -0700

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Label2_Click(sender As System.Object, e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        frmMain.Show()
+        Me.Hide()
+    End Sub
+End Class
